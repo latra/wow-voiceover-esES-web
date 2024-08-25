d070373c8e5ffb69dc77e600113bd047ba85a1f6
Latra Gallucci <personadelmonton@gmail.com>
2024-08-25 13:42:08 +0200

    readme

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className="description">
         <p>¡Azeroth te necesita, héroe! Debido a la grandeza de World of Warcraft, nos resulta imposible verificar que todas las voces y audios se estén generando correctamente.</p>
         <p>Toda ayuda cuenta, por lo que agradecemos que nos ayudéis a identificar las voces que están dando más problemas a través de esta web para que podamos centrar esfuerzos en mejorar aquellas que dan peor resultado.</p>
-        <p>Si por el contrario la voz se escucha bien pero una misión o interacción concreta da problemas, por favor, cread una Issue de GitHub <a href="https://github.com/tu-repositorio/issues">aquí</a> para que podamos revisar y corregir ese audio manualmente.</p>
+        <p>Si por el contrario la voz se escucha bien pero una misión o interacción concreta da problemas, por favor, cread una Issue de GitHub <a href="https://github.com/latra/wow-webvoices-esES/issues/new?assignees=&labels=bug&projects=&template=audio_bug.md&title=%5BAUDIO%5D+Audio+incorrecto">aquí</a> para que podamos revisar y corregir ese audio manualmente.</p>
       </div>
       <div className="voice-list">
         {voicesData.voices.map((voice, index) => (
